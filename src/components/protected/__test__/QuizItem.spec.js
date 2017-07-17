f97a562f2c8297d883f680fa078df1854bbfc571
mtreerungroj <m.treerungroj@outlook.com>
2017-07-17 15:15:55 +0700

    Test QuizItem component

--- src/components/protected/__test__/QuizItem.spec.js
+++ src/components/protected/__test__/QuizItem.spec.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import React from 'react'
+import { shallow, mount, render } from 'enzyme'
 import QuizItem, { Element } from '../QuizItem'
+import IconButton from 'material-ui/IconButton'
 
-describe('QuizItem component test', () => {
+describe('QuizItem component', () => {
   const quest = {}
-  const func = () => { }
+  const func = () => {}
 
   it('should render Element component', () => {
-    const Wrapper = shallow(<QuizItem quest={quest} editQuiz={func} deleteQuiz={func} />)
-    expect(Wrapper.contains(<Element quest={quest} deleteQuiz={func} editQuiz={func} />)).toEqual(true)
+    const Wrapper = shallow(
+      <QuizItem idx={0} quest={quest} editQuiz={func} deleteQuiz={func} />
+    )
+    expect(
+      Wrapper.contains(
+        <Element idx={0} quest={quest} editQuiz={func} deleteQuiz={func} />
+      )
+    ).toEqual(true)
   })
 
-  it('Element component should render QuizInput component and button', () => {
-    const Wrapper = shallow(<Element quest={quest} deleteQuiz={func} editQuiz={func} />)
+  it('Element component should render QuizInput component and delete button', () => {
+    const Wrapper = shallow(
+      <Element idx={0} quest={quest} deleteQuiz={func} editQuiz={func} />
+    )
     expect(Wrapper.find('QuizInput')).toHaveLength(1)
-    expect(Wrapper.find('button')).toHaveLength(1)
+    expect(Wrapper.find('IconButton')).toHaveLength(1)
   })
 })
