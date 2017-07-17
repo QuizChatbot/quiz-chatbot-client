bde8f5c5f39c055d58c46c4f7b87150276f662bf
mtreerungroj <m.treerungroj@outlook.com>
2017-07-17 15:14:41 +0700

    Test Leaderboard component

--- src/components/__test__/Leaderboard.spec.js
+++ src/components/__test__/Leaderboard.spec.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-import Leaderboard, { LeaderboardItem } from '../Leaderboard'
+import React from 'react'
+import { shallow, mount, render } from 'enzyme'
+import Leaderboard from '../Leaderboard'
 
-describe('Leaderboard component test', () => {
+describe('Leaderboard component', () => {
   const props = {
-    developers: [{
-      profile: {
-        first_name: 'Xiao',
-        last_name: 'Ying'
-      },
-      maxSummary: {
-        grade: 'B',
-        score: 75
+    developers: [
+      {
+        profile: {
+          first_name: 'Manusaporn',
+          last_name: 'Treerungroj',
+          profile_pic: 'pic_src'
+        },
+        '12_factors_app': {
+          grade: 'A',
+          score: '80'
+        }
       }
-    }]
+    ]
   }
 
-  it('should render leaderbaord item message', () => {
-    const Wrapper = shallow(<LeaderboardItem developer={props.developers[0]} key={0} idx={0} />)
-    expect(Wrapper.contains(<h2>1. Xiao Ying B ( score: 75 )</h2>)).toEqual(true)
+  it('should render button for choose category', () => {
+    const Wrapper = shallow(<Leaderboard {...props} />)
+    expect(Wrapper.find('CategoryButton')).toHaveLength(2)
   })
 
-  it('should render leaderboard component', () => {
+  it('should render LeaderboardItem component', () => {
     const Wrapper = shallow(<Leaderboard {...props} />)
     expect(Wrapper.contains(<h2>Leaderboard</h2>)).toEqual(true)
-    expect(Wrapper.contains(<LeaderboardItem developer={props.developers[0]} key={0} idx={0} />)).toEqual(true)
+    expect(Wrapper.find('LeaderboardItem')).toHaveLength(1)
   })
 
-  it('should render No players message if developers length is 0', () => {
+  it('should render No Players message if developers length is 0', () => {
     const Wrapper = shallow(<Leaderboard developers={[]} />)
     expect(Wrapper.contains('No Players')).toEqual(true)
   })
