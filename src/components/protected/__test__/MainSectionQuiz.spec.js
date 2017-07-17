18f86f3f3f4be6dc18cf3f8e7bb74366ac1286a9
mtreerungroj <m.treerungroj@outlook.com>
2017-07-17 15:16:10 +0700

    Test MainSectionQuiz component

--- src/components/protected/__test__/MainSectionQuiz.spec.js
+++ src/components/protected/__test__/MainSectionQuiz.spec.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow, mount, render } from 'enzyme'
 import MainSectionQuiz, { Footer } from '../MainSectionQuiz'
 
-describe('MainSectionQuiz component test', () => {
+describe('MainSectionQuiz component', () => {
   const actions = {}
 
   it('should render No Quizzes message if no quiz', () => {
@@ -10,16 +10,20 @@ describe('MainSectionQuiz component test', () => {
     const Wrapper = shallow(
       <MainSectionQuiz quests={quests} actions={actions} />
     )
-    expect(Wrapper.contains(<div>No Quizzes</div>)).toEqual(true)
+    expect(
+      Wrapper.contains(
+        <div><br />You have no quizzes, try adding some quiz now!</div>
+      )
+    ).toEqual(true)
   })
 
-  it('should render QuizItem component if have quiz', () => {
+  it('should render QuizItem container if have quiz', () => {
     const quests = [{ id: 1 }, { id: 2 }]
     const Wrapper = shallow(
       <MainSectionQuiz quests={quests} actions={actions} />
     )
-    expect(Wrapper.find('QuizItem')).toHaveLength(2)
     expect(Wrapper.find(Footer)).toHaveLength(1)
+    expect(Wrapper.find('QuizItem')).toHaveLength(2)
   })
 
   it('Footer should render QuizCount component', () => {
