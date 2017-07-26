12aec470a2aa88d23d195f049a9d8ab2f2ddc2d0
mtreerungroj <m.treerungroj@outlook.com>
2017-07-26 16:05:30 +0700

    Add privacy policy link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
 
+const getStyle = {
+  p: {
+    margin: '0px auto',
+    padding: '0px',
+    color: 'rgba(255, 255, 255, 0.54)'
+  }
+}
 export class Footer extends Component {
   render () {
     return (
@@ -10,13 +17,7 @@ export class Footer extends Component {
           textAlign: 'center'
         }}
       >
-        <p
-          style={{
-            margin: '0px auto',
-            padding: '0px',
-            color: 'rgba(255, 255, 255, 0.54)'
-          }}
-        >
+        <p style={getStyle.p}>
           Hand crafted with love by our awesome {' '}
           <a
             href='https://github.com/QuizChatbot/quiz-chatbot/graphs/contributors'
@@ -26,13 +27,7 @@ export class Footer extends Component {
           </a>
           .
         </p>
-        <p
-          style={{
-            margin: '0px auto',
-            padding: '0px',
-            color: 'rgba(255, 255, 255, 0.54)'
-          }}
-        >
+        <p style={getStyle.p}>
           Repository on {' '}
           <a
             href='https://github.com/QuizChatbot/quiz-chatbot'
@@ -43,6 +38,16 @@ export class Footer extends Component {
           .
         </p>
 
+        <br />
+        <p style={getStyle.p}>
+          <a
+            href='https://termsfeed.com/privacy-policy/c8fc5cfdb5a8f798dd59bf4b8d91a1b6'
+            style={{ color: 'rgba(255, 255, 255, 0.87)' }}
+          >
+            Privacy policy
+          </a>
+          {' '} of Quiz Chatbot.
+        </p>
       </div>
     )
   }
